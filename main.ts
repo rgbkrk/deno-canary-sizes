340e4d88557f708c1bf22333bac9e40fea9a9715
Kyle Kelley <rgbkrk@gmail.com>
2023-10-17 15:35:59 -0700

    demonstrate notebook to deno deploy

--- main.ts
+++ main.ts
@@ -59,10 +59,9 @@ async function readNotebook(location: string): Promise<Notebook> {
   return nbjson;
 }
 
-const notebookName = "./canary-sizes.ipynb";
-const notebook = await readNotebook(notebookName);
-
 Deno.serve(async (_req) => {
+  const notebookName = "./canary-sizes.ipynb";
+  const notebook = await readNotebook(notebookName);
   let html = `<html><head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.3.0/github-markdown.min.css">
